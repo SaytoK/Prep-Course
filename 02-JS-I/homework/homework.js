17a02b28fd46c9323a53922da13c0ed7f43862d0
SaytoK <nr81019@gmail.com>
2022-02-23 21:01:20 -0300

    11 problems resolved

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -25,34 +25,38 @@ const nuevoModulo = 21 % 5 === 1;
 // Pista: "console.log()" NO fucionará.
 // No cambies los nombres de las funciones.
 
-function devolverString(str) {
+function devolverString(srt) {
   // "Return" la string provista: str
   // Tu código:
-  
+  return srt
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
+return (x + y)
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
+  return (x - y)
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
+  return (x * y)
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
+  return (x / y)
   
 }
 
