c71d0b84d904e115b1847da41d9417416b1a3121
SaytoK <nr81019@gmail.com>
2022-02-23 21:02:18 -0300

    11 problems resolved

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -35,28 +35,28 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-return (x + y)
+return (x + y);
   
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  return (x - y)
+  return (x - y);
   
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return (x * y)
+  return (x * y);
   
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  return (x / y)
+  return (x / y);
   
 }
 
@@ -64,6 +64,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  return 
   
 }
 
